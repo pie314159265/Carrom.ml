cb9db7b2fcd2c317627c4dbecabcfbb82b6f7a7b
susw12 <sujaymswain@outlook.com>
2016-12-05 19:33:00 -0500

    Add files via upload
    New Files
    Daniel this is what u need

---
+++ carrom.ml (1)/Assets/BallPhysics.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+public var pos = vector2(0, 0);
+public float CONTAINMENT = .5;
+public float speed = 0;
+public float dir = 0;
+public float r = self.CircleCollider2D.radius;
+
+}
+
+function Update () {
+
+}
+
+boolean GetInside() {
+
+}
