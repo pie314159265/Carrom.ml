264383ea5e8f5c0795233c74a0d9a9d6e1ef143c
susw12 <susw12@users.noreply.github.com>
2016-12-09 00:02:25 -0500

    Update UnityProgress.js

--- Images/UnityProgress.js
+++ Images/UnityProgress.js
@@ -12,19 +12,19 @@ function UnityProgress (dom) {
 	this.background = background;
 
 	var logoImage = document.createElement("img");
-	logoImage.src = "TemplateData/progresslogo.png"; 
+	logoImage.src = "https://cdn.rawgit.com/susw12/Carrom.ml/site/Images/progresslogo.png"; 
 	logoImage.style.position = "absolute";
 	parent.appendChild(logoImage);
 	this.logoImage = logoImage;
 
 	var progressFrame = document.createElement("img");
-	progressFrame.src = "TemplateData/loadingbar.png"; 
+	progressFrame.src = "https://cdn.rawgit.com/susw12/Carrom.ml/site/Images/loadingbar.png"; 
 	progressFrame.style.position = "absolute";
 	parent.appendChild(progressFrame);
 	this.progressFrame = progressFrame;
 
 	var progressBar = document.createElement("img");
-	progressBar.src = "TemplateData/fullbar.png"; 
+	progressBar.src = "https://cdn.rawgit.com/susw12/Carrom.ml/site/Images/fullbar.png"; 
 	progressBar.style.position = "absolute";
 	parent.appendChild(progressBar);
 	this.progressBar = progressBar;
