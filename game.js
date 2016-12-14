3ac7c164507040b4c7cc38de5c4d5ba3dfa24552
IlluminatiMan <danielmonroe0606@gmail.com>
2016-12-14 16:31:42 -0500

    Created game.js and merged files

--- game.js
+++ game.js
@@ -1,9 +1,99 @@
+class user {
+	constructor(name, passcode, wins, losses, rating) {
+		var self = this
+		var self.name = name;
+		var self.passcode = passcode
+		temp = 0 || wins
+		var self.wins = temp;
+		temp = 0 || losses;
+		var self.losses = temp;
+		temp = 1200 || rating
+		var self.rating = temp;
+		del temp;
+		self.updateData()
+}
+
+	eval_game(win, opp_rating) {
+
+		if win === true {
+			increase = 1.8 ** (opp_rating / self.rating) * 7 + 10;
+			this.rating += increase;
+			del increase;
+			this.wins += 1
+}
+
+		else {
+			decrease = 1.8 ** (opp_rating / self.rating) * 6 + 8;
+			this.rating -= decrease;
+			del decrease;
+			this.losses += 1;
+
+}
+
+
+
+}
+
+	updateData() {
+		//return name + ' ' + str(wins) + ' ' + lo
+		
+		
+}
+}
+
+
+
+
 class Game () {
 	function constructor (username) {
-		var self = this
-		self.username = username
-		users = readFile()
-		for (user in users) {
+		var self = this;
+		self.LEADERLENGTH = 20;
+		self.name = username;
+		self.user = -1;
+		updateUsers();
+		if self.user == -1 {
+			for (user in self.users) {
+				if user.name = self.name {
+					self.user = user;
+				}
+				
+				
+			}
+			
+			if user === -1 {
+				alert('user not found');
+			}
+		}
+		
+				
+			
+			
+		
+	
+	function updateUsers()	{
+		self.users = readFile();
+
+	}	
+			
+	function getLeaderboard () {
+		updateUsers();
+		leaderboard = [];
+		for (user in self.users) {
+			if user.rating > leaderboard[leaderboard.length-1] {
+				del leaderboard[leaderboard.length-1];
+				
+				for (index=1, index < leaderboard.length, index ++) {
+					if user.rating < leaderboard[index].rating {
+						leaderboard.splice(index + 1, 0, user) //insert after
+						break;
+					}
+					
+				}
+			}
+		}
+		
+		
+	}
     
     
     }
